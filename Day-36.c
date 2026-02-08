3c1cab8e461ae3582ef051b39debffa240bf0514
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-02-08 10:43:13 +0600

    Update Day-36.c

--- Day-36.c
+++ Day-36.c
@@ -117,10 +117,74 @@ default : printf("Invalid Grade");
 
 
 
+
+
+//Problem 4: Month Days Counter
+
+int month;
+printf("Enter any month number : ");
+scanf("%d",&month);
+
+switch(month){
+
+case 1:
+printf("31 Days");
+break;
+
+case 2:
+printf("28 Days");
+break;
+
+case 3:
+printf("30 Days");
+break;
+
+case 4:
+printf("31 Days");
+break;
+
+case 5:
+printf("30 Days");
+break;
+
+case 6:
+printf("31 Days");
+break;
+
+case 7:
+printf("30 Days");
+break;
+
+case 8:
+printf("31 Days");
+break;
+
+case 9:
+printf("30 Days");
+break;
+
+case 10:
+printf("31 Days");
+break;
+
+case 11:
+printf("30 Days");
+break;
+
+case 12:
+printf("31 Days");
+break;
+
+default: printf("Invalid Month");
+
+}
+    
+
     
 
     return 0;
 
 }
+
 
 
